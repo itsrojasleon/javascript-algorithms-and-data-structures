193e582fe7398206e9952f45849cab73c3c7b314
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-06-02 19:54:07 -0500

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+module.exports = {
+  title: 'Javascript algorithms and data structures',
+  tagline:
+    'Become a better developer and get ready for your next coding interview',
+  url: 'https://notsure.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Javascript',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg'
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left'
+        },
+        {
+          href: 'https://github.com/rojasleon/something',
+          label: 'GitHub',
+          position: 'right'
+        }
+      ]
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/'
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/'
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Github',
+              href: 'https://github.com/rojasleon'
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/rojas_leon_'
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} rojasleon super hyper corporation, Inc. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
+  ]
+};
